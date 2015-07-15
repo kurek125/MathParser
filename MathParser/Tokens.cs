cc221bf7ef831d14c2d5bfa2dceb786c9e15810d
Kurek125 <kurator125@hotmail.com>
2015-07-15 17:27:21 +0200

    Added new functions

--- MathParser/Tokens.cs
+++ MathParser/Tokens.cs
@@ -44,11 +44,48 @@ namespace MathExpressions
                 new Token() {action = Div,Precedence = 2, Symbol = "/"},
                 new Token() {action = Pow,Precedence = 3, Symbol = "^"},
 
+                //
+                new Token() {action = Percentage,Precedence = 9,Symbol = "%"},
+                new Token() {action = Factorial,Precedence = 9,Symbol = "!"},
+                new Token() {action = Exponential,Precedence = 9,Symbol = "exp"},
+                new Token() {action = Round,Precedence = 9,Symbol = "round"},
+                new Token() {action = Abs,Precedence = 9,Symbol = "abs"},
+                new Token() {action = Sign,Precedence = 9,Symbol = "sign"},
+
+                //logarithm
+                new Token() {action = Logn,Precedence = 9,Symbol = "ln"},
+                new Token() {action = Log10,Precedence = 9,Symbol = "log10"},
+
                 //trigonometric
                 new Token() {action = Sin,Precedence = 9, Symbol = "sin"},
                 new Token() {action = Cos,Precedence = 9, Symbol = "cos"},
                 new Token() {action = Tan,Precedence = 9, Symbol = "tan"},
                 new Token() {action = Cot,Precedence = 9, Symbol = "cot"},
+                new Token() {action = Sec,Precedence = 9,Symbol = "sec"},
+                new Token() {action = Cosec,Precedence = 9,Symbol = "cosec"},
+
+                //cyclometric
+                new Token() {action = Arcsin,Precedence = 9,Symbol = "arcsin"},
+                new Token() {action = Arccos,Precedence = 9,Symbol = "arccos"},
+                new Token() {action = Arctan,Precedence = 9,Symbol = "arctan"},
+                new Token() {action = Arccot,Precedence = 9,Symbol = "arccot"},
+                new Token() {action = Arcsec,Precedence = 0,Symbol = "arcsec"},
+                new Token() {action = Arccosec,Precedence = 9,Symbol = "arccosec"},
+                //cyclometric alias
+                new Token() {action = Arcsin,Precedence = 9,Symbol = "asin"},
+                new Token() {action = Arccos,Precedence = 9,Symbol = "acos"},
+                new Token() {action = Arctan,Precedence = 9,Symbol = "atan"},
+                new Token() {action = Arccot,Precedence = 9,Symbol = "acot"},
+                new Token() {action = Arcsec,Precedence = 0,Symbol = "asec"},
+                new Token() {action = Arccosec,Precedence = 9,Symbol = "acosec"},
+
+                //hyperbolic
+                new Token() {action = Sinh,Precedence = 9,Symbol = "sinh"},
+                new Token() {action = Cosh,Precedence = 9,Symbol = "cosh"},
+                new Token() {action = Tanh,Precedence = 9,Symbol = "tanh"},
+                new Token() {action = Coth,Precedence = 9,Symbol = "coth"},
+                new Token() {action = Sech,Precedence = 0,Symbol = "sech"},
+                new Token() {action = Cosech,Precedence = 9,Symbol = "cosech"},
 
                 //const
                 new Token() {action = ConstPi,Precedence = 99,Symbol = "PI"},
@@ -118,6 +155,67 @@ namespace MathExpressions
             operands.Push(Math.Pow(firstArg, secondArg));
         }
 
+        //
+        private void Percentage(Stack<double> operands, bool useRadians)
+        {
+            var arg = operands.Pop();
+            operands.Push(arg / 100.0);
+        }
+        private void Factorial(Stack<double> operands, bool useRadians)
+        {
+            var arg = operands.Pop();
+            double result = 1.0;
+
+            if (arg <= 1)
+            {
+                operands.Push(result);
+            }
+            else
+            {
+                for (int i = 2; i <= arg; i++)
+                {
+                    result *= i;
+                }
+
+                operands.Push(result);
+            }
+        }
+        private void Exponential(Stack<double> operands, bool useRadians)
+        {
+            var arg = operands.Pop();
+            operands.Push(Math.Exp(arg));
+        }
+        private void Round(Stack<double> operands, bool useRadians)
+        {
+            var arg = operands.Pop();
+            operands.Push(Math.Round(arg));
+        }
+        private void Abs(Stack<double> operands, bool useRadians)
+        {
+            var arg = operands.Pop();
+            operands.Push(Math.Abs(arg));
+        }
+        private void Sign(Stack<double> operands, bool useRadians)
+        {
+            var arg = operands.Pop();
+            operands.Push(Math.Sign(arg));
+        }
+        //todo: Truncate
+        //todo: Floor
+        //todo: Ceiling
+
+        //logarithm
+        private void Logn(Stack<double> operands, bool useRadians)
+        {
+            var arg = operands.Pop();
+            operands.Push(Math.Log(arg));
+        }
+        private void Log10(Stack<double> operands, bool useRadians)
+        {
+            var arg = operands.Pop();
+            operands.Push(Math.Log10(arg));
+        }
+
         //trigonometric
         private void Sin(Stack<double> operands, bool useRadians)
         {
@@ -139,5 +237,96 @@ namespace MathExpressions
             var arg = useRadians ? operands.Pop() : (operands.Pop() * Math.PI / 180.0);
             operands.Push(1.0 / Math.Tan(arg));
         }
+        private void Sec(Stack<double> operands, bool useRadians)
+        {
+            var arg = useRadians ? operands.Pop() : (operands.Pop() * Math.PI / 180.0);
+            operands.Push(1.0 / Math.Cos(arg));
+        }
+        private void Cosec(Stack<double> operands, bool useRadians)
+        {
+            var arg = useRadians ? operands.Pop() : (operands.Pop() * Math.PI / 180.0);
+            operands.Push(1.0/Math.Sin(arg));
+        }
+        
+
+        //cyclometric
+        private void Arcsin(Stack<double> operands, bool useRadians)
+        {
+            var arg = operands.Pop();
+            operands.Push(
+                useRadians ? Math.Asin(arg) : Math.Asin(arg) *180.0 /Math.PI
+                );
+        }
+        private void Arccos(Stack<double> operands, bool useRadians)
+        {
+            var arg = operands.Pop();
+            operands.Push(
+                useRadians ? Math.Acos(arg) : Math.Acos(arg) * 180.0 / Math.PI
+                );
+        }
+        private void Arctan(Stack<double> operands, bool useRadians)
+        {
+            var arg = operands.Pop();
+            operands.Push(
+                useRadians ? Math.Atan(arg) : Math.Atan(arg) * 180.0 / Math.PI
+                );
+        }
+        private void Arccot(Stack<double> operands, bool useRadians)
+        {
+            var arg = operands.Pop();
+            operands.Push(
+                useRadians ? Math.Atan(arg) : Math.Atan(arg) * 180.0 / Math.PI
+                );
+        }
+        private void Arcsec(Stack<double> operands, bool useRadians)
+        {
+            var arg = operands.Pop();
+            operands.Push(
+                useRadians ? Math.Acos(1.0/arg) : Math.Acos(1.0/arg) * 180.0 / Math.PI
+                );
+        }
+        private void Arccosec(Stack<double> operands, bool useRadians)
+        {
+            var arg = operands.Pop();
+            operands.Push(
+                useRadians ? Math.Asin(1.0/arg) : Math.Asin(1.0/arg) * 180.0 / Math.PI
+                );
+        }
+
+        //hyperbolic
+        private void Sinh(Stack<double> operands, bool useRadians)
+        {
+            var arg = useRadians ? operands.Pop() : (operands.Pop() * Math.PI / 180.0);
+            operands.Push(Math.Sinh(arg));
+        }
+        private void Cosh(Stack<double> operands, bool useRadians)
+        {
+            var arg = useRadians ? operands.Pop() : (operands.Pop() * Math.PI / 180.0);
+            operands.Push(Math.Cosh(arg));
+        }
+        private void Tanh(Stack<double> operands, bool useRadians)
+        {
+            var arg = useRadians ? operands.Pop() : (operands.Pop() * Math.PI / 180.0);
+            operands.Push(Math.Tanh(arg));
+        }
+        private void Coth(Stack<double> operands, bool useRadians)
+        {
+            var arg = useRadians ? operands.Pop() : (operands.Pop() * Math.PI / 180.0);
+            operands.Push(1.0/Math.Tanh(arg));
+        }
+        private void Sech(Stack<double> operands, bool useRadians)
+        {
+            var arg = useRadians ? operands.Pop() : (operands.Pop() * Math.PI / 180.0);
+            operands.Push(1.0/Math.Cosh(arg));
+        }
+        private void Cosech(Stack<double> operands, bool useRadians)
+        {
+            var arg = useRadians ? operands.Pop() : (operands.Pop() * Math.PI / 180.0);
+            operands.Push(1.0 / Math.Sinh(arg));
+        }
+
+        //todo: Gudermannian function
+        //todo: Error function
+
     }
 }
